d3380dce0db50bbbb308783f863e93ff8fb366f3
XuanYing <lucy40087amy75@gmail.com>
2020-10-23 17:19:22 +0800

    Update sw.js

--- page/sw.js
+++ page/sw.js
@@ -1,21 +1,10 @@
 // 當service worker在「安裝階段」時會觸發此事件
-self.addEventListener('install', event => {
-  event.waitUntil(
-    .then(cache => cache.addAll([
-      'index.html'
-    ]))
-  )
-})
+self.addEventListener('install', function(event) {
+    console.log('[Service Worker] Installing Service Worker ...', event);
+});
 
 //載入外部JS、CSS的CDN或是內部的其他檔案資源時觸發
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function (response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  )
-})
+self.addEventListener('fetch', function(event) {
+    console.log('[Service Worker] Fetch something ...', event);
+    event.respondWith(fetch(event.request));
+});
