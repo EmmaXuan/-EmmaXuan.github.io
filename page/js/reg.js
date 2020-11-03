0527a644049e1d39f5d003377b41a4315140c457
Emma <lucy40087amy75@gmail.com>
2020-11-03 17:27:18 +0800

    add reg.js

---
+++ page/js/reg.js
@@ -0,0 +1,5 @@
+ if('serviceWorker' in navigator) {   // 檢查browser有無支援serviceWorker
+    navigator.serviceWorker.register('sw.js').then(function() {   // register會回傳一個Promise
+        console.log('Service worker registered!');
+    });
+  }
